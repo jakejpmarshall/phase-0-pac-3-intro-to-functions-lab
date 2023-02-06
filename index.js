88d1cc400e051f9040bce6812968af990123eed3
jakejpmarshall@gmail.com <jakemarshall@Jacobs-MacBook-Air.local>
2023-02-06 16:07:39 -0700

    functions lab

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function shout(string) {
+    return string.toUpperCase();
+}
+shout('Hello');
+function whisper(string) {
+     return string.toLowerCase();
+}
+
+whisper("Hello")   
+
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+logShout('Hello');
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+logWhisper('Hello');
+
+let yeet;
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return 'YES INDEED!';
+    } else if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    } else {
+      return;
+    }
+  }
